97f79083e89ecd926e1061e6018db15b56b3b17a
Patrice Cadiot <pathibul.r@gmail.com>
2019-12-05 16:20:47 +0100

    day00 améliore l'ex07

--- day00/ex07/filterwords.py
+++ day00/ex07/filterwords.py
@@ -6,12 +6,8 @@ def operate(string, size):
     return [word for word in string.split(' ') if len(word) > size]
 
 if __name__ == '__main__':
-    if len(argv) == 3 and not argv[1].isdigit():
-        try:
-            print(operate(argv[1], int(argv[2])))
-        except (AttributeError, ValueError):
-            print("ERROR")
-            exit(1)
+    if len(argv) == 3 and not argv[1].isdigit() and argv[2].isdigit():
+        print(operate(argv[1], int(argv[2])))
     else:
         print("ERROR")
         exit(1)
