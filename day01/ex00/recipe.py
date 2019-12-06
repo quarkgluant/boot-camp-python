891458dd4a45acba8abb178151510346e3175f1a
Patrice Cadiot <pathibul.r@gmail.com>
2019-12-06 17:16:10 +0100

    day01 WIP ex00, recipe.py finished, book.py and test.py in progress

--- day01/ex00/recipe.py
+++ day01/ex00/recipe.py
@@ -1,55 +1,9 @@
-def controler_types(*a_args, **a_kwargs):
-    """On attend en paramètres du décorateur les types souhaités. On accepte
-    une liste de paramètres indéterminés, étant donné que notre fonction
-    définie pourra être appelée avec un nombre variable de paramètres et que
-    chacun doit être contrôlé"""
-
-    def decorateur(fonction_a_executer):
-        """Notre décorateur. Il doit renvoyer fonction_modifiee"""
-
-        def fonction_modifiee(*args, **kwargs):
-            """Notre fonction modifiée. Elle se charge de contrôler
-            les types qu'on lui passe en paramètres"""
-
-            # La liste des paramètres attendus (a_args) doit être de même
-            # Longueur que celle reçue (args)
-            if len(a_args) != len(args):
-                raise TypeError("le nombre d'arguments attendu n'est pas égal " \
-                                "au nombre reçu")
-            # On parcourt la liste des arguments reçus et non nommés
-            for i, arg in enumerate(args):
-                if a_args[i] is not type(args[i]):
-                    raise TypeError("l'argument {0} n'est pas du type " \
-                                    "{1}".format(i, a_args[i]))
-
-            # On parcourt à présent la liste des paramètres reçus et nommés
-            for cle in kwargs:
-                if cle not in a_kwargs:
-                    raise TypeError("l'argument {0} n'a aucun type " \
-                                    "précisé".format(repr(cle)))
-                if a_kwargs[cle] is not type(kwargs[cle]):
-                    raise TypeError("l'argument {0} n'est pas de type" \
-                                    "{1}".format(repr(cle), a_kwargs[cle]))
-            return fonction_a_executer(*args, **kwargs)
-
-        return fonction_modifiee
-
-    return decorateur
-
-
-@controler_types(int, int)
-def intervalle(base_inf, base_sup):
-    print("Intervalle de {0} à {1}".format(base_inf, base_sup))
-   
-intervalle(1, 8)
-# Intervalle de 1 à 8
-intervalle(5, "oups!")
-# Traceback (most recent call last):
-#   File "<stdin>", line 1, in <module>
-#   File "<stdin>", line 24, in fonction_modifiee
-# TypeError: l'argument 1 n'est pas du type <class 'int'>
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
 
 def Recipe():
+    """class Recipe with name, cooking_lvl, cooking_time, ingredients, description, recipe_type"""
+
     def __init__(self, name, cooking_lvl, cooking_time, ingredients, description, recipe_type):
         self._name = name
         self._cooking_lvl = cooking_lvl
