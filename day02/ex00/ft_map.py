2b07cfb2db8af1ef3f8cf6ccd641b1ca6350eae3
Patrice Cadiot <pathibul.r@gmail.com>
2019-12-13 15:59:18 +0100

    day02 ex00 done

--- day02/ex00/ft_map.py
+++ day02/ex00/ft_map.py
@@ -2,4 +2,21 @@
 # -*-coding:utf-8 -*
 
 def ft_map(function_to_apply, list_of_inputs):
-    pass
+    return (function_to_apply(element) for element in list_of_inputs)
+
+if __name__ == '__main__':
+    my_strings = ['a', 'b', 'c', 'd', 'e']
+    my_numbers = [1,2,3,4,5]
+
+    results = list(map(str.upper, my_strings))
+    ft_results = list(ft_map(str.upper, my_strings))
+
+    print(results)
+    print(ft_results)
+    print(results == ft_results)
+    my_pets = ['alfred', 'tabitha', 'william', 'arla']
+
+    uppered_pets = list(map(str.upper, my_pets))
+    ft_uppered_pets = list(ft_map(str.upper, my_pets))
+
+    print(uppered_pets == ft_uppered_pets)
--- day02/ex00/ft_reduce.py
+++ day02/ex00/ft_reduce.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*
+from functools import reduce
+
 
 def ft_reduce(function_to_apply, list_of_inputs):
-    pass
+    res = list_of_inputs[0]
+    res = function_to_apply(res, list_of_inputs[1])
+    for i in range(2, len(list_of_inputs)):
+        res = function_to_apply(res, list_of_inputs[i])
+
+    return res
+
+
+if __name__ == '__main__':
+    my_numbers = [1,2,3,4,5]
+
+    results = reduce(lambda x, y: x * y, my_numbers)
+    ft_results = ft_reduce(lambda x, y: x * y, my_numbers)
+
+    print(results)
+    print(ft_results)
+    print(results == ft_results)
+
+    print(reduce(lambda a, b: a * b, range(1, 11)))
+    print(ft_reduce(lambda a, b: a * b, range(1, 11)))
