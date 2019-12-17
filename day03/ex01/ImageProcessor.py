8aa74d4a750962e17fe1881950f47611efdd0a9e
Patrice Cadiot <pathibul.r@gmail.com>
2019-12-17 21:46:11 +0100

    day03 ex01 done

---
+++ day03/ex01/ImageProcessor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+
+import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ImageProcessor:
+    def load(self, path) :
+        """opens the .png file specified by the path argument and returns an array
+        with the RGB values of the image pixels. It must display a message specifying
+        the dimensions of the image (e.g. 340 x 500)."""
+        img = mpimg.imread(path)
+        # if img.dtype == np.float32:  # Si le résultat n'est pas un tableau d'entiers
+        #     img = (img * 255).astype(np.uint8)
+        print(f"Loading image of dimensions {img.shape[0:2]}")
+        return img
+
+    def display(self, array) :
+        """takes a NumPy array as an argument and displays the corresponding RGB image."""
+        plt.imshow(array)
+        plt.show()
+
+if __name__ == '__main__':
+    # from ImageProcessor import ImageProcessor
+    imp = ImageProcessor()
+    arr = imp.load("../ressources/42AI.png")
+    # Loading image of dimensions 200 x 200
+    imp.display(arr)
+    print(arr)
