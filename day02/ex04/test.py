d9650fefff9b7ef087063aa34c1cc177c8a85970
Patrice Cadiot <pathibul.r@gmail.com>
2019-12-17 19:24:13 +0100

    day02 ex04 almost done but no module found in the package day03 initial commit

--- day02/ex04/test.py
+++ day02/ex04/test.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*
-from time import sleep
-
-import ai42.progressbar
-import ai42.logging.log
-
-@log
-def on_progresse:
-    listy = range(3333)
-    ret = 0
-    for elem in progressbar(listy):
-        ret += elem
-        sleep(0.005)
-    print()
-    print(ret)
-
-if __name__ == '__main__':
-    on_progresse()
+# from time import sleep
+#
+# import ai42.progressbar
+# import ai42.logging.log
+#
+# @log
+# def on_progresse:
+#     listy = range(3333)
+#     ret = 0
+#     for elem in progressbar(listy):
+#         ret += elem
+#         sleep(0.005)
+#     print()
+#     print(ret)
+#
+# if __name__ == '__main__':
+#     on_progresse()
