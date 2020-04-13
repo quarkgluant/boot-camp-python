c53621e374d62c38b14c5d0a4f7bbb3b566db4b4
Patrice Cadiot <pathibul.r@gmail.com>
2020-04-13 16:39:05 +0200

    day00 reformat ex10

--- day00/ex10/loading.py
+++ day00/ex10/loading.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8 -*
 
+import sys
+import time
 from time import sleep
-import sys, time
 
 
 def ft_progress(list, prefix="", size=60, file=sys.stdout):
@@ -15,7 +16,7 @@ def ft_progress(list, prefix="", size=60, file=sys.stdout):
         total_time = (time.time() - start) * count if j == 0 else (time.time() - start) * (count / j)
         eta = abs(total_time - (time.time() - start))
         file.write("ETA: %02.2fs [%02.2f%s] %s[%s%s%s] %i/%i | elapsed time %02.2fs\r" % (
-        eta, percent, "%", prefix, "=" * x, ">", " " * (size - x), j, count, (time.time() - start)))
+            eta, percent, "%", prefix, "=" * x, ">", " " * (size - x), j, count, (time.time() - start)))
         file.flush()
 
     show(0)
@@ -26,34 +27,19 @@ def ft_progress(list, prefix="", size=60, file=sys.stdout):
     file.flush()
 
 
-listy = range(3333)
-ret = 0
-for elem in ft_progress(listy):
-    ret += elem
-    sleep(0.005)
-print()
-print(ret)
+if __name__ == '__main__':
+    listy = range(3333)
+    ret = 0
+    for elem in ft_progress(listy):
+        ret += elem
+        sleep(0.005)
+    print()
+    print(ret)
 
-listy = range(1000)
-ret = 0
-for elem in ft_progress(listy):
-    ret += (elem + 3) % 5
-    sleep(0.01)
-print()
-print(ret)
-
-
-def progressbar(it, prefix="", size=60, file=sys.stdout):
-    count = len(it)
-
-    def show(j):
-        x = int(size * j / count)
-        file.write("%s[%s%s] %i/%i\r" % (prefix, "#" * x, "." * (size - x), j, count))
-        file.flush()
-
-    show(0)
-    for i, item in enumerate(it):
-        yield item
-        show(i + 1)
-    file.write("\n")
-    file.flush()
+    listy = range(1000)
+    ret = 0
+    for elem in ft_progress(listy):
+        ret += (elem + 3) % 5
+        sleep(0.01)
+    print()
+    print(ret)
